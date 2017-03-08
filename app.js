578b9041dea04f4ebd81c74ca40de954e8a0d246
iancstewart <ian.c.stewart@icloud.com>
2017-03-08 16:45:14 +0100

    :sparkles: Introducing chat feature.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
   const appSettings = {
     urlFundaSearch(type, query, page, size) {return `http://funda.kyrandia.nl/feeds/Aanbod.svc/json/${config.FUNDA_KEY}/?type=${type}&zo=/${query}/&page=${page}&pagesize=${size}`;},
     urlGetAddress(lat, long) {return `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${config.GOOGLE_KEY}&result_type=street_address|locality`;},
-    chat: document.querySelector('#chat'),
+    chat: document.querySelector('#chat-body'),
     error: document.querySelector('.error')
   };
 
@@ -17,8 +17,125 @@
 
   const routes = {
     listen() {
-      store.hydrate();
+      chat.listen();
       window.addEventListener('hashchange', () => section.toggle(window.location.hash), false);
+    }
+  };
+
+  const chat = {
+    listen() {
+      this.nextQuestion();
+      document.querySelector('#chat-form').addEventListener('submit', this.submit, false);
+    },
+    submit(event) {
+      event.preventDefault();
+      const input = document.querySelector('#chat-input');
+      chat.awnserQuestion(input.value);
+      input.value = '';
+    },
+    awnserQuestion(response) {
+      appSettings.chat.innerHTML += `
+        <section>
+          <header><h1>user</h1></header>
+          <p>${response}</p>
+        </section>
+      `;
+      this.nextQuestion(response);
+    },
+    nextQuestion(response) {
+      let html;
+
+      switch (this.questionCount) {
+        case 0:
+          html = chat.questions[chat.questionCount];
+          chat.questionCount = chat.questionCount + 1;
+          break;
+        case 1:
+          if(response.toLowerCase() === 'koop' || response.toLowerCase() === 'huur') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+            chat.userChoices.type = response.toLowerCase();
+          } else {
+            html = chat.awnsers[chat.questionCount - 1];
+          }
+          break;
+        case 2:
+          if(response.toLowerCase() === 'woonhuis' || response.toLowerCase() === 'appartement') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+            chat.userChoices.options.push(response.toLowerCase());
+          } else if(response.toLowerCase() === 'maakt niet uit') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+          } else {
+            html = chat.awnsers[chat.questionCount - 1];
+          }
+          break;
+        case 3:
+          if(response.toLowerCase() === 'balkon' || response.toLowerCase() === 'dakterras' || response.toLowerCase() === 'tuin') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+            chat.userChoices.options.push(response.toLowerCase());
+          } else if(response.toLowerCase() === 'maakt niet uit') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+          } else {
+            html = chat.awnsers[chat.questionCount - 1];
+          }
+          break;
+        case 4:
+          if(response.toLowerCase() === 'nieuwbouw' || response.toLowerCase() === 'bestaande bouw') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+            chat.userChoices.options.push(response.toLowerCase());
+            store.hydrate();
+          } else if(response.toLowerCase() === 'maakt niet uit') {
+            html = chat.questions[chat.questionCount];
+            chat.questionCount = chat.questionCount + 1;
+            store.hydrate();
+          } else {
+            html = chat.awnsers[chat.questionCount - 1];
+          }
+          break;
+        case 5:
+          if(response.toLowerCase() === 'ja') {
+            html = chat.questions[0];
+            chat.questionCount = 0;
+            chat.userChoices = [];
+          } else if(response.toLowerCase() === 'nee') {
+            html = 'Oke top! Als je je nog bedenkt stuur dan even "ja" 😇';
+          } else {
+            html = chat.awnsers[chat.questionCount - 1];
+          }
+          break;
+      }
+
+      this.questionCount <= 5
+      ? appSettings.chat.innerHTML += `
+        <section>
+          <header><h1>Fundapi</h1></header>
+          <p>${html}</p>
+        </section>`
+      : null;
+    },
+    questionCount: 0,
+    questions: [
+      'zoekt u een "koop" huis of een "huur" huis?',
+      'Zoek u naar een "woonhuis" of een "appartement"?',
+      'Wat voor buiten ruimte is gewenst?',
+      '"Nieuwbouw" of "Bestaande bouw"?',
+      'Ik ga de huizen ophalen die voldoen aan u criteria'
+    ],
+    awnsers: [
+      'Ik snap niet wat je bedoeld, antwoord met "koop" of "huur".',
+      'Ik snap niet wat je bedoeld, antwoord met "woonhuis", "appartament" of "maakt niet uit".',
+      'Ik snap niet wat je bedoeld, antwoord met "balkon", "dakterras", "tuin" of "maakt niet uit".',
+      'Ik snap niet wat je bedoeld, antwoord met "Nieuwbouw", "Bestaande bouw" of "maakt niet uit".',
+      'Ik snap niet wat je bedoeld, antwoord met "ja" of "nee".'
+    ],
+    userChoices: {
+      type: '',
+      options: []
     }
   };
 
@@ -49,6 +166,7 @@
       : appSettings.chat.innerHTML += `
         <p>Theres no houses in this area "${data.Metadata.Omschrijving}"</p>
       `;
+      appSettings.chat.innerHTML += 'Wilt u opnieuw beginnen?';
     },
     error(error) {
       let html = error ? error : 'seems like something went wrong';
